0cc22c761d9ac25bbb7fed39a1396628d5792d1c
erkutbas <ebas1@binghamton.edu>
2017-12-07 19:27:31 +0300

    ufak değişiklik

--- Project Version 0_1/Extensions/ControllerExtensions/LoginViewExtensionFunction/SwiftKeyChainExtensionFunctions.swift
+++ Project Version 0_1/Extensions/ControllerExtensions/LoginViewExtensionFunction/SwiftKeyChainExtensionFunctions.swift
@@ -19,6 +19,8 @@ extension LoginViewController {
     
     func readKeyChainDataForRememberMeProcess() -> Bool {
         
+        print("readKeyChainDataForRememberMeProcess starts")
+        
         if let resultRememberMe = KeychainWrapper.standard.bool(forKey: STRING_KEY_REMEMBER_ME) {
             
             return resultRememberMe
@@ -32,32 +34,49 @@ extension LoginViewController {
     
     func registerdRememberMeUserData(inputEmail : String, inputPassword : String) {
         
+        print("registerdRememberMeUserData starts")
+        
         if self.checkBoxButton.isChecked == true {
+            
+            print("yarro 1")
             
             KeychainWrapper.standard.set(inputEmail, forKey: STRING_KEY_EMAIL)
             KeychainWrapper.standard.set(inputPassword, forKey: STRING_KEY_PASSWORD)
+            KeychainWrapper.standard.set(true, forKey: STRING_KEY_REMEMBER_ME)
             
         } else {
             
+            print("yarro 2")
+            
             KeychainWrapper.standard.removeObject(forKey: STRING_KEY_EMAIL)
             KeychainWrapper.standard.removeObject(forKey: STRING_KEY_PASSWORD)
+            KeychainWrapper.standard.removeObject(forKey: STRING_KEY_EMAIL)
         }
         
         
         
     }
     
-    
     func setRememberMeEnabledOrDisabled() {
+        
+        print("setRememberMeEnabledOrDisabled starts")
         
         if readKeyChainDataForRememberMeProcess() {
             
             self.checkBoxButton.isChecked = true
+            getUserEmailAndPassword()
             
         } else {
             
             self.checkBoxButton.isChecked = false
         }
+        
+    }
+    
+    func getUserEmailAndPassword() {
+        
+        self.email.text = KeychainWrapper.standard.string(forKey: STRING_KEY_EMAIL)
+        self.password.text = KeychainWrapper.standard.string(forKey: STRING_KEY_PASSWORD)
         
     }
     
