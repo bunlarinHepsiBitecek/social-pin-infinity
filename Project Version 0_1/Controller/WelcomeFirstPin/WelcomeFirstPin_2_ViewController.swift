8a46c5682f84d5015011d97984cd9f9ed2e11452
erkutbas <ebas1@binghamton.edu>
2017-12-16 16:46:28 +0300

    Buttonlar eklendi

---
+++ Project Version 0_1/Controller/WelcomeFirstPin/WelcomeFirstPin_2_ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  WelcomeFirstPin_2_ViewController.swift
+//  Project Version 0_1
+//
+//  Created by Erkut Baş on 16.12.2017.
+//  Copyright © 2017 Erkut Baş. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class WelcomeFirstPin_2_ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.disableNavigationBar()
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print("LocationManager Starts.......")
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.mapView.setRegion(region, animated: true)
+        
+    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        if overlay is MKPolyline {
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blue
+            polylineRenderer.lineWidth = 4
+            return polylineRenderer
+        }
+        return nil
+    }
+    
+
+}
