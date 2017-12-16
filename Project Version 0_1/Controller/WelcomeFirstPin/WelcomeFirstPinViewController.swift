ca442ca81816d5747017d3b10cc16ad02fe4ccba
erkutbas <ebas1@binghamton.edu>
2017-12-16 16:48:00 +0300

    Ufak değişiklik

--- Project Version 0_1/Controller/WelcomeFirstPin/WelcomeFirstPinViewController.swift
+++ Project Version 0_1/Controller/WelcomeFirstPin/WelcomeFirstPinViewController.swift
@@ -1,61 +1,51 @@
 //
-//  WelcomeFirstPinViewController.swift
+//  WelcomeFirstPin_2_ViewController.swift
 //  Project Version 0_1
 //
-//  Created by Erkut Baş on 15.12.2017.
+//  Created by Erkut Baş on 16.12.2017.
 //  Copyright © 2017 Erkut Baş. All rights reserved.
 //
 
 import UIKit
 import MapKit
 
-class WelcomeFirstPinViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
+class WelcomeFirstPinViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-    @IBOutlet var theMap: MKMapView!
+    @IBOutlet var mapView: MKMapView!
     
-    var manager:CLLocationManager!
-    var myLocations: [CLLocation] = []
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        manager = CLLocationManager()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestAlwaysAuthorization()
-        manager.startUpdatingLocation()
-        
-        //Setup our Map View
-        theMap.delegate = self
-        theMap.mapType = MKMapType.mutedStandard
-        theMap.showsUserLocation = true
-        
+        self.navigationController?.disableNavigationBar()
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        mapView.delegate = self
+        mapView.showsUserLocation = true
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[AnyObject]) {
-        myLocations.append(locations[0] as! CLLocation)
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        let spanX = 10.0
-        let spanY = 10.0
-        var newRegion = MKCoordinateRegion(center: theMap.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
-        theMap.setRegion(newRegion, animated: true)
+        print("LocationManager Starts.......")
         
-        if (myLocations.count > 1){
-            var sourceIndex = myLocations.count - 1
-            var destinationIndex = myLocations.count - 2
-            
-            let c1 = myLocations[sourceIndex].coordinate
-            let c2 = myLocations[destinationIndex].coordinate
-            var a = [c1, c2]
-            var polyline = MKPolyline(coordinates: &a, count: a.count)
-            theMap.add(polyline)
-        }
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.mapView.setRegion(region, animated: true)
         
     }
     
@@ -70,10 +60,5 @@ class WelcomeFirstPinViewController: UIViewController, CLLocationManagerDelegate
         return nil
     }
     
-    @IBAction func deneme(_ sender: Any) {
-        
-        performSegue(withIdentifier: "deneme", sender: self)
-        
-    }
-    
+
 }
