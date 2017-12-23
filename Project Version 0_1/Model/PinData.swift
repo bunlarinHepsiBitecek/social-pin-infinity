d37e0627d4155980c1723081d54368d62b743a5a
erkutbas <ebas1@binghamton.edu>
2017-12-23 14:20:23 +0300

    remzi başkan için

---
+++ Project Version 0_1/Model/PinData.swift
@@ -0,0 +1,25 @@
+//
+//  PinData.swift
+//  Project Version 0_1
+//
+//  Created by Erkut Baş on 23.12.2017.
+//  Copyright © 2017 Erkut Baş. All rights reserved.
+//
+
+import UIKit
+
+/*
+class PinData {
+    
+    private var user : User
+    private var location : Location
+    private var pictureOnPin : UIImage
+    private var textOnPin : String
+    //private var videoOnPin :
+    
+    init() {
+        
+        
+        
+    }
+}*/
